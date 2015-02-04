3b95f74786657234ee66c51e47e52cb1bc4e8aea
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-02-04 23:16:25 +0000

    adding more stuff

--- Pam.hs
+++ Pam.hs
@@ -1,68 +1,19 @@
-import Data.String.Utils (join)
-import System.Random (getStdGen)
-import Control.Monad.Random.Class (MonadRandom)
+import BinTree (BinTree (Node, Empty), treeFromList, subTree)
+import Data.Monoid
 
-data BinTree a = Empty | Node a (BinTree a) (BinTree a)
+runModel :: BinTree a -> BinTree a -> BinTree a
+runModel init potential = potential
 
-instance Functor BinTree where
-    fmap _ Empty = Empty
-    fmap f (Node a left right) = Node (f a) (fmap f left) (fmap f right)
+sumNeighs :: (Monoid a) => BinTree a -> BinTree a
+sumNeighs = sumNeighs' mempty
 
-split :: [a] -> ([a], [a])
-split [] = ([], [])
-split (x:xs) = let ~(e, o) = split xs in (x:o, e)
-
-treeFromList :: [a] -> BinTree a
-treeFromList [] = Empty
-treeFromList (x:xs) = let (e, o) = split xs in Node x (treeFromList e) (treeFromList o)
-
-myTree = treeFromList [0..]
-
-sublist :: Int -> Int -> [a] -> [a]
-sublist a b t = drop a . take b $ t
-
-fstIndx :: Int -> Int
-fstIndx depth = 2^depth - 1
-
-lstIndx :: Int -> Int
-lstIndx depth = 2^(depth + 1) - 1
-
-subTree :: Int -> BinTree a -> BinTree a
-subTree 0 _ = Empty
-subTree n Empty = Empty
-subTree n (Node x l r) = Node x (subTree (n-1) l) (subTree (n-1) r)
-
-
-instance (Show a) => Show (BinTree a) where
-    show t = show' 0 treeList
-        where
-            maxDepth = 5
-            treeList :: [String]
-            treeList = map show (listFromTree t)
-            showDepth :: Int -> [String] -> String
-            showDepth depth t = join " "
-                (sublist (fstIndx depth) (lstIndx depth) t) ++ "\n"
-            show' :: Int -> [String] -> String
-            show' depth treeList
-                | fstIndx depth > length treeList = ""
-                | depth > maxDepth = "..."
-                | otherwise        = showDepth depth treeList
-                    ++ show' (depth + 1) treeList
-
-listFromTree :: BinTree a -> [a]
-listFromTree tree = bf [tree]
+sumNeighs' :: (Monoid a) => a -> BinTree a -> BinTree a
+sumNeighs' parentVal Empty = Empty
+sumNeighs' parentVal (Node x l r) = Node (parentVal `mappend` nodeVal l `mappend` nodeVal r) (sumNeighs' x l) (sumNeighs' x r)
     where
-        bf :: [BinTree a] -> [a]
-        bf [] = []
-        bf xs = map nodeValue xs ++ bf (concatMap children xs)
-        nodeValue :: BinTree a -> a
-        nodeValue (Node a _ _) = a
-        children (Node _ Empty Empty) = []
-        children (Node _ Empty b) = [b]
-        children (Node _ a Empty) = [a]
-        children (Node _ a b) = [a, b]
+        nodeVal :: (Monoid a) => BinTree a -> a
+        nodeVal (Node x _ _) = x
+        nodeVal Empty = mempty
 
 main :: IO ()
-main = do
-        gen <- MonadRandom [1, 2]
-        print gen
+main = print . subTree 5 $ treeFromList [0..]

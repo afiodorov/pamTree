e0a145d260ba2940d636eaa48adf8aca7d8c17d4
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-02-06 00:56:18 +0000

    tree can add notes

--- Pam.hs
+++ Pam.hs
@@ -1,19 +1,23 @@
 import BinTree (BinTree (Node, Empty), treeFromList, subTree)
 import Data.Monoid
+import Control.Applicative
 
 runModel :: BinTree a -> BinTree a -> BinTree a
 runModel init potential = potential
 
-sumNeighs :: (Monoid a) => BinTree a -> BinTree a
-sumNeighs = sumNeighs' mempty
+sumNeighs ::  (Num a) => BinTree a -> BinTree a
+sumNeighs t = getSum <$> mconcatNeighs' mempty (fmap Sum t)
 
-sumNeighs' :: (Monoid a) => a -> BinTree a -> BinTree a
-sumNeighs' parentVal Empty = Empty
-sumNeighs' parentVal (Node x l r) = Node (parentVal `mappend` nodeVal l `mappend` nodeVal r) (sumNeighs' x l) (sumNeighs' x r)
+mconcatNeighs' :: (Monoid a) => a -> BinTree a -> BinTree a
+mconcatNeighs' parentVal Empty = Empty
+mconcatNeighs' parentVal (Node x l r) = Node
+    (mconcat [parentVal,  nodeVal l, nodeVal r]) (mconcatNeighs' x l) (mconcatNeighs' x r)
     where
         nodeVal :: (Monoid a) => BinTree a -> a
         nodeVal (Node x _ _) = x
         nodeVal Empty = mempty
 
 main :: IO ()
-main = print . subTree 5 $ treeFromList [0..]
+main = do
+    print . sumNeighs . subTree 5 $ treeFromList [0 ..]
+    print . subTree 5 $ treeFromList [0 ..]
